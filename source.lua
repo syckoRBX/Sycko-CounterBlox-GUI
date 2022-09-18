6cafc5897b9b51276faf98994a01bb54623bc765
syckoRBX <113863308+syckoRBX@users.noreply.github.com>
2022-09-18 19:28:29 -0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,3 +1,3 @@
 --Source code is replaced to other path. Visit https://github.com/anim9r/BloxFun/blob/main/code.lua
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/anim9r/BloxFun/main/code.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/syckoRBX/Sycko-CounterBlox-GUI/main/code.lua"))()
