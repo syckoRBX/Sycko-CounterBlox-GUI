f722a7d604b5fde953d01a5be831bd9176e152f2
syckoRBX <113863308+syckoRBX@users.noreply.github.com>
2022-09-18 19:27:35 -0300

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,3 @@
+--Source code is replaced to other path. Visit https://github.com/anim9r/BloxFun/blob/main/code.lua
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/anim9r/BloxFun/main/code.lua"))()
